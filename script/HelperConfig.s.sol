d1933bb78d51bbb667a199e957d8f60952666b93
marvycodes <olowomarve@gmail.com>
2024-09-09 22:39:45 +0100

    add mockAggregator

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -9,6 +9,8 @@ import {MockV3Aggregator} from "../test/mocks/MockV3Aggregator.sol";
 contract HelperConfig  is Script{
 
     NetworkConfig public activeNetworkConfig;
+    uint8 public constant DECIMALS = 8;
+    int256 public constant  INITIAL_PRICE = 2000e8;
 
     struct NetworkConfig {
         address priceFeed; 
@@ -18,9 +20,9 @@ contract HelperConfig  is Script{
         if (block.chainid == 11155111) {
             activeNetworkConfig = getSepoliaEthConfig();
         }
-        // else (
-        //     // activeNetworkConfig = getAnvilEthConfig();
-        // )
+        else {
+            activeNetworkConfig = getorCreateAnvilEthConfig();
+        }
     }
 
 
@@ -33,9 +35,12 @@ contract HelperConfig  is Script{
         return sepoliaConfig;
     }
 
-    function getAnvilEthConfig() public returns (NetworkConfig memory){
+    function getorCreateAnvilEthConfig() public returns (NetworkConfig memory){
+        if (activeNetworkConfig.priceFeed != address(0)) {
+            return activeNetworkConfig;
+        }
         vm.startBroadcast();
-        MockV3Aggregator mockPriceFeed = new MockV3Aggregator(8, 2000e8);
+        MockV3Aggregator mockPriceFeed = new MockV3Aggregator(DECIMALS, INITIAL_PRICE );
         vm.stopBroadcast();
 
         NetworkConfig memory anvilConfig = NetworkConfig({
